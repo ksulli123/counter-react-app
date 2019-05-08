08523e0e9451b1ce7b7113ba46b8847fb8003cdc
ksulli123 <r-kensullivan@outlook.com>
2019-05-08 22:17:20 +1000

    version with tags

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,17 +2,27 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
   };
 
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
   render() {
-    this.getBadgeClasses();
     return (
       <>
-        <span style={{ fontSize: 30 }} className={this.getBadgeClasses()}>
-          {this.formatCount()}
-        </span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        {this.renderTags()}
       </>
     );
   }
